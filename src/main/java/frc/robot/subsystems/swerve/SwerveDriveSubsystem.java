fe088b1ebcdeafdbaeb1b9195cf53412bf847b7d
Oliverlin67 <thoblinoliver@gmail.com>
2024-03-09 20:25:56 +0800

    save

--- src/main/java/frc/robot/subsystems/swerve/SwerveDriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/swerve/SwerveDriveSubsystem.java
@@ -12,6 +12,7 @@ import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.RobotState;
 import edu.wpi.first.wpilibj.SerialPort.Port;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -262,7 +263,7 @@ public class SwerveDriveSubsystem extends SubsystemBase {
             getModulePositions()
         );
 
-        if(DataContainer.camPose != null) {
+        if(DataContainer.camPose != null && !RobotState.isAutonomous()) {
             setPose(DataContainer.camPose.toPose2d());
         }
 
